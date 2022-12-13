97758bf7a55c780b8aedab8175fd4c534a0aab2b
Babalola Muhammad <barmilitary@yahoo.com>
2022-12-13 12:04:57 +0100

    m

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://dczars-api.herokuapp.com',
+      target: 'https://hotel-u0ef.onrender.com',
       // target: 'http://localhost:4000',
       changeOrigin: true,
     })
